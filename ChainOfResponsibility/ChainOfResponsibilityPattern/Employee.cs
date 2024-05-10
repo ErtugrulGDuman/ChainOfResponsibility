07696ddf30bf14d8c0bfa413a235946b44fb8d26
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-05-10 20:36:46 +0300

    Add project files.

---
+++ ChainOfResponsibility/ChainOfResponsibilityPattern/Employee.cs
@@ -0,0 +1,14 @@
+﻿using ChainOfResponsibility.Models;
+
+namespace ChainOfResponsibility.ChainOfResponsibilityPattern
+{
+    public abstract class Employee
+    {
+        protected Employee NextApprover;
+        public void SetNextApprover(Employee employee)
+        {
+            this.NextApprover = employee;
+        }
+        public abstract void ProcessRequest(CustomerViewModel customerViewModel);
+    }
+}
---
+++ ChainOfResponsibility/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChainOfResponsibility.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=EGD\\SQLEXPRESS;Initial Catalog=ChainOfDb;integrated security=true");
+        }
+        public DbSet<CustomerProcess> CustomerProcess { get; set; }
+    }
+}
